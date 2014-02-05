9712406533c283ac4272cb00d07217fa720c7759
Vernalis-Developers <vernalis-developers@localhost>
2014-02-05 15:39:46 +0000

    Updating to 1.0.4 with revised url length

---
+++ com.vernalis.knime/src/com/vernalis/nodes/fasta/FASTASequenceNodeFactory_2.java
@@ -0,0 +1,74 @@
+/*
+ * ------------------------------------------------------------------------
+ *  Copyright (C) 2013, Vernalis (R&D) Ltd
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ * ------------------------------------------------------------------------
+ */
+package com.vernalis.nodes.fasta;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+
+/**
+ * <code>NodeFactory</code> for the "FASTASequence" Node.
+ * Extract the chains and sequences from a PDB FASTA sequence file column
+ */
+public class FASTASequenceNodeFactory_2 
+        extends NodeFactory<FASTASequenceNodeModel_2> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public FASTASequenceNodeModel_2 createNodeModel() {
+        return new FASTASequenceNodeModel_2();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getNrNodeViews() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeView<FASTASequenceNodeModel_2> createNodeView(final int viewIndex,
+            final FASTASequenceNodeModel_2 nodeModel) {
+        return new FASTASequenceNodeView_2(nodeModel);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hasDialog() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return new FASTASequenceNodeDialog_2();
+    }
+
+}
+
