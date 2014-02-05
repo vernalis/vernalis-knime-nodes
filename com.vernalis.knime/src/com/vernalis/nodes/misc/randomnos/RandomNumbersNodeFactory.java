73386bcd221b39218a58ace5a08afcbc9e55e5c7
Vernalis-Developers <vernalis-developers@localhost>
2014-02-05 15:28:45 +0000

    Updating to 1.0.4 with revised url length

---
+++ com.vernalis.knime/src/com/vernalis/nodes/misc/randomnos/RandomNumbersNodeFactory.java
@@ -0,0 +1,72 @@
+/*
+ * ------------------------------------------------------------------------
+ *  Copyright (C) 2013, Vernalis (R&D) Ltd
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ * ------------------------------------------------------------------------
+ */
+package com.vernalis.nodes.misc.randomnos;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeFactory</code> for the "RandomNumbers" Node.
+ */
+public class RandomNumbersNodeFactory 
+        extends NodeFactory<RandomNumbersNodeModel> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public RandomNumbersNodeModel createNodeModel() {
+        return new RandomNumbersNodeModel();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getNrNodeViews() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeView<RandomNumbersNodeModel> createNodeView(final int viewIndex,
+            final RandomNumbersNodeModel nodeModel) {
+        return new RandomNumbersNodeView(nodeModel);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hasDialog() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return new RandomNumbersNodeDialog();
+    }
+
+}
+
