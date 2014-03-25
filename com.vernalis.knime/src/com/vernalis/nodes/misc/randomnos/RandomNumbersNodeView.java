e4eee34eb2b6f51b4e82d2aece4278783c0aa5dc
Vernalis-Developers <vernalis-developers@localhost>
2014-03-25 16:40:13 +0000

    v 1.1.2

---
+++ com.vernalis.knime/src/com/vernalis/nodes/misc/randomnos/RandomNumbersNodeView.java
@@ -0,0 +1,63 @@
+/*
+ * ------------------------------------------------------------------------
+ *  Copyright (C) 2013, Vernalis (R&D) Ltd
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ * ------------------------------------------------------------------------
+ */
+package com.vernalis.nodes.misc.randomnos;
+
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeView</code> for the "RandomNumbers" Node.
+ * A node to generate a table with a single column of random numbers
+ */
+public class RandomNumbersNodeView extends NodeView<RandomNumbersNodeModel> {
+
+    /**
+     * Creates a new view.
+     * 
+     * @param nodeModel The model (class: {@link RandomNumbersNodeModel})
+     */
+    protected RandomNumbersNodeView(final RandomNumbersNodeModel nodeModel) {
+        super(nodeModel);
+        // TODO: generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void modelChanged() {
+        // TODO: generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onClose() {
+        // TODO: generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onOpen() {
+        // TODO: generated method stub
+    }
+
+}
+
