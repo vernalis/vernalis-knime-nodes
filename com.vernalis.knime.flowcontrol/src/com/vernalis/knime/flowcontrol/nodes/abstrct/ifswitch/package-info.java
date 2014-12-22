5849196d00422f251669b7c1dfa46bb917d4523c
Vernalis-Developers <vernalis-developers@localhost>
2014-12-22 15:16:23 +0000

    Updating to v 1.3.0

--- com.vernalis.knime.flowcontrol/src/com/vernalis/knime/flowcontrol/nodes/abstrct/ifswitch/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides {@link com.vernalis.knime.flowcontrol.nodes.abstrct.ifswitch.AbstractIfSwitchNodeDialog} and
- * {@link com.vernalis.knime.flowcontrol.nodes.abstrct.ifswitch.AbstractIfSwitchNodeModel} classes for
- * IF Switch node implementations
- */
-package com.vernalis.knime.flowcontrol.nodes.abstrct.ifswitch;
