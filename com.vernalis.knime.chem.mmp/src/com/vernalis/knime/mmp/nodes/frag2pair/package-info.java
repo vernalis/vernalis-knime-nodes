62881bb310f647eee2f6ffbb6692d7ed6b007a14
Vernalis-Developers <vernalis-developers@localhost>
2015-03-12 14:43:44 +0000

    v1.3.4 (NB Supercedes previous incorrect v1.3.3 update with same details) 	-	Added Jameed's MMP Fragmentation rSMARTS 	-	Added User Defined rSMARTS option 	-	Added Include Reverse Transform option 	-	Added Include rSMARTS for transform in output 	-	Improved memory management (Explicit deleting or RDKit objects) 	-	Enhanced node-cancelling capabality during fragmentation process

---
+++ com.vernalis.knime.chem.mmp/src/com/vernalis/knime/mmp/nodes/frag2pair/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides the Classes for implementation of the Fragments to Matched Molecular Pairs node
+ */
+package com.vernalis.knime.mmp.nodes.frag2pair;
---
+++ com.vernalis.knime.flowcontrol/src/com/vernalis/knime/flowcontrol/nodes/abstrct/caseselect/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides Abstract 
+ * {@link com.vernalis.knime.flowcontrol.nodes.abstrct.caseselect.AbstractCaseSelectNodeDialog} and 
+ * {@link com.vernalis.knime.flowcontrol.nodes.abstrct.caseselect.AbstractCaseSelectNodeModel} 
+ * classes for the CASE select node implementations
+ */
+package com.vernalis.knime.flowcontrol.nodes.abstrct.caseselect;
