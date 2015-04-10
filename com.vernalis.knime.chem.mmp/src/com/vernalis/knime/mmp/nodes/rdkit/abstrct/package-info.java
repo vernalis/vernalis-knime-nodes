9584d1a5e86c04dd114503c88490465f2b3d90c9
Vernalis-Developers <vernalis-developers@localhost>
2015-04-10 14:26:32 +0000

    Updated to v1.3.5 Added improved RDKit importing Added Attachment Point Fingerprints

--- com.vernalis.knime.chem.mmp/src/com/vernalis/knime/mmp/nodes/rdkit/abstrct/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides Abstract classes for the RDKIT MMP node implementations
- */
-package com.vernalis.knime.mmp.nodes.rdkit.abstrct;
--- com.vernalis.knime.chem.mmp/src/com/vernalis/knime/mmp/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides Data containers and fragmentation types and methods for Matched Molecular Pair related nodes.
- */
-package com.vernalis.knime.mmp;
