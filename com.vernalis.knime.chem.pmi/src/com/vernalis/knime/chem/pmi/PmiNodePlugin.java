314902198b6960218a33cd237a1faee0b7d6d72b
Vernalis-Developers <vernalis-developers@localhost>
2016-04-13 10:28:54 +0000

    Initial commit of pmi plugin

--- com.vernalis.knime.chem.pmi/src/com/vernalis/knime/chem/pmi/PmiNodePlugin.java
+++ com.vernalis.knime.chem.pmi/src/com/vernalis/knime/chem/pmi/PmiNodePlugin.java
@@ -1,17 +1,17 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2015, Vernalis (R&D) Ltd
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License, Version 3, as 
- *  published by the Free Software Foundation.
+ * Copyright (c) 2016, Vernalis (R&D) Ltd
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License, Version 3, as 
+ * published by the Free Software Foundation.
+ * 
+ *  This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
  *  
- *   This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of 
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
- *  See the GNU General Public License for more details.
- *   
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, see <http://www.gnu.org/licenses>
- *******************************************************************************/
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>
+ ******************************************************************************/
 package com.vernalis.knime.chem.pmi;
 
 import org.eclipse.core.runtime.Plugin;
