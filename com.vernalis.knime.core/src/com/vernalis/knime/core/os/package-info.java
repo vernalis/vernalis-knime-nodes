8accade307d9f10abdd1af6bcde8ef49ff3425b6
Vernalis-Developers <vernalis-developers@localhost>
2017-01-25 14:09:26 +0000

    Updated to version 1.8.3
    Fixed bug in SpeedySMILES filter/splitter nodes resulting in missing cell throwing NullPointerException
    
    Removed invalid html characters from javadoc

--- com.vernalis.knime.core/src/com/vernalis/knime/core/os/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains Operating-system related classes
- */
-package com.vernalis.knime.core.os;
--- com.vernalis.knime.io/src/com/vernalis/knime/io/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains the plugin activator for the Miscellaneous IO plugin
- */
-package com.vernalis.knime.io;
