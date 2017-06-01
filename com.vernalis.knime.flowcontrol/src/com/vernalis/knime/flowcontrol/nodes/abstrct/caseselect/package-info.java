6dc8e8ee5b910b750f4bbe417bf93ecf006997c5
Vernalis-Developers <vernalis-developers@localhost>
2017-06-01 13:52:56 +0000

    Upate to 1.8.4 Fixed bugs in MMP nodes introduced by updates to RDKit: https://github.com/rdkit/rdkit/pull/1202 https://github.com/rdkit/rdkit/pull/1190
    Added additional test cases to MMPFragment.* testflows

--- com.vernalis.knime.flowcontrol/src/com/vernalis/knime/flowcontrol/nodes/abstrct/caseselect/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Provides Abstract 
- * {@link com.vernalis.knime.flowcontrol.nodes.abstrct.caseselect.AbstractCaseSelectNodeDialog} and 
- * {@link com.vernalis.knime.flowcontrol.nodes.abstrct.caseselect.AbstractCaseSelectNodeModel} 
- * classes for the CASE select node implementations
- */
-package com.vernalis.knime.flowcontrol.nodes.abstrct.caseselect;
