63e3d70b2c221dadc6abf8c5481ddf2ca02051d0
Vernalis-Developers <vernalis-developers@localhost>
2017-06-22 14:59:04 +0000

    Updated to v.1.9.0
    * PDB Downloader nodes deprecated
    * New PDB Downloader nodes introduced with more options
    * New versions dont fail on missing data
    * URLs for PDB downloads fixed following changes on server
    * FileHelper readURLToString method attempts to follow 301 return type redirects

--- com.vernalis.knime.flowcontrol/src/com/vernalis/knime/flowcontrol/nodes/abstrct/fvendswitch/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Provides the {@link com.vernalis.knime.flowcontrol.nodes.abstrct.fvendswitch.AbstractFlowVarEndSwitchNodeModel}
- * for flow variable port End If/Case nodes
- */
-package com.vernalis.knime.flowcontrol.nodes.abstrct.fvendswitch;
