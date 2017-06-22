cd67578e5d96fdea6e3fd38c80bf27a08b48b025
Vernalis-Developers <vernalis-developers@localhost>
2017-06-22 14:59:30 +0000

    Updated to v.1.9.0
    * PDB Downloader nodes deprecated
    * New PDB Downloader nodes introduced with more options
    * New versions dont fail on missing data
    * URLs for PDB downloads fixed following changes on server
    * FileHelper readURLToString method attempts to follow 301 return type redirects

---
+++ com.vernalis.knime.perfmon/src/com/vernalis/knime/perfmon/nodes/loopend/timing/memory/PerfMemMonTiming1PortEndNodeFactory.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2016, Vernalis (R&D) Ltd
+ *  This program is free software; you can redistribute it and/or modify it 
+ *  under the terms of the GNU General Public License, Version 3, as 
+ *  published by the Free Software Foundation.
+ *  
+ *   This program is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>
+ ******************************************************************************/
+package com.vernalis.knime.perfmon.nodes.loopend.timing.memory;
+
+import com.vernalis.knime.perfmon.nodes.timing.abstrct.AbstractPerfMemMonTimingEndNodeFactory;
+
+/**
+ * <code>NodeFactory</code> for the 1 port timing/memory monitoring loop end
+ *
+ * @author S. Roughley
+ */
+public class PerfMemMonTiming1PortEndNodeFactory extends AbstractPerfMemMonTimingEndNodeFactory {
+
+	public PerfMemMonTiming1PortEndNodeFactory() {
+		super(1);
+	}
+
+}
