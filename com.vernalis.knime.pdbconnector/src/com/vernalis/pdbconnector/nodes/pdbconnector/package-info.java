197a0bd4e99ac728defec870806a0a4f53e8a689
Vernalis-Developers <vernalis-developers@localhost>
2017-08-10 13:57:54 +0000

    Update to v1.12.0 Added fingerprint nodes plugin and retired original fingerprint properties node

--- com.vernalis.knime.pdbconnector/src/com/vernalis/pdbconnector/nodes/pdbconnector/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- * This package contains the classes for the node implementations for the 2nd
- * Generation PDB Connector node family
- * 
- * @author S.Roughley
- *
- */
-package com.vernalis.pdbconnector.nodes.pdbconnector;
