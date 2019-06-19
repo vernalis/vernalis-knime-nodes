f137297907f323a4066a07858a9cbd9e4ce22f21
Vernalis <vernalis@localhost>
2019-06-19 11:27:31 +0000

    Update to v.1.20.3
    Update version constraints to [3.0.0,5.0.0) to accommodate update to KNIME v4.0.0

--- com.vernalis.knime.flowcontrol/src/com/vernalis/knime/flowcontrol/nodes/abstrct/varvalifswitch/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides {@link com.vernalis.knime.flowcontrol.nodes.abstrct.varvalifswitch.AbstractFvvalIfSwitchNodeDialog}
- * and {@link com.vernalis.knime.flowcontrol.nodes.abstrct.varvalifswitch.AbstractFvvalIfSwitchNodeModel}
- * shared classes for Flow Variable Value IF Switch nodes
- */
-package com.vernalis.knime.flowcontrol.nodes.abstrct.varvalifswitch;
