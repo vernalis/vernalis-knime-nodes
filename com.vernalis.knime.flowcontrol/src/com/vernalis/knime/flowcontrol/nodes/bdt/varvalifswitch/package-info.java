dc7f63fb7b8b36a2acea2519dcd0caf875615f38
Vernalis <vernalis@localhost>
2020-03-17 16:42:04 +0000

    Update to v1.26.0
    Bug-fixes and minor enhancements to PDB Connector family of nodes:
    * Fixed broken date query fields (Deposition Date, Release Date,
    Revision Date)
    * Added searches for Entity IDs, Chain IDs, Uniprot Gene Names
    * Fixed mouse scroll speed

--- com.vernalis.knime.flowcontrol/src/com/vernalis/knime/flowcontrol/nodes/bdt/varvalifswitch/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides the {@link com.vernalis.knime.flowcontrol.nodes.bdt.varvalifswitch.FvvalIfSwitchNodeFactory}
- * Factory class for the {@link org.knime.core.node.BufferedDataTable} implementation of the Flow Variable
- * Value IF Switch Node
- */
-package com.vernalis.knime.flowcontrol.nodes.bdt.varvalifswitch;
