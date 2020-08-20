cc52b748121fe2f7f1c3005f121d990036466a89
Vernalis <vernalis@localhost>
2020-08-20 11:57:07 +0000

    Update to v1.27.0
    New Nodes
    ----------
    * Append to Collection
    * Insert into List
    * Credential Variable Extractor (see https://forum.knime.com/t/credentials-input-node-extract-username/25079)
    
    Bux Fixes / Enhancements
    ---------
    * PMI Properties node now works with two-character element symbols e.g. Cl, Br etc (see https://forum.knime.com/t/vernalis-pmi-node-failure/24979/28)
    * List Folders node no longer throws NullPointerException (see https://forum.knime.com/t/id-like-to-download-files-from-s3-to-knime-server/23289/5?u=s.roughley)
    * Memory monitoring performance loop nodes re-written to remove possible IOExceptions due to table ownership of memory usage table
    * Performance loop end nodes now allow use of new or legacy date-time systems in timing and memory usage output tables
    * PMI calculation errors no longer causes node execution failure
    * SettingsModelIntegerRannge#createClone() now returns correct type
    * Added all missing version constraints in MANIFEST.MF

--- com.vernalis.knime.flowcontrol/src/com/vernalis/knime/flowcontrol/nodes/abstrct/endswitch/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides abstract {@link com.vernalis.knime.flowcontrol.nodes.abstrct.endswitch.AbstractEndSwitchNodeDialog}
- * and {@link com.vernalis.knime.flowcontrol.nodes.abstrct.endswitch.AbstractEndSwitchNodeModel} classes
- * for shared code for the End If/Case nodes
- */
-package com.vernalis.knime.flowcontrol.nodes.abstrct.endswitch;
