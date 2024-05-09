fb99c23e18d234662578a951255353269fbfa71e
sroughley <s.roughley@vernalis.com>
2024-05-09 10:42:27 +0100

    Update to v1.36.6
    * Added Missing value and uniqueness options to Collection Size node
    (see https://forum.knime.com/t/vernalis-collection-size-add-option-to-ignore-missing/78983?u=vernalis)

--- com.vernalis.knime/src/com/vernalis/nodes/collection/size/CollectionSizeNodeDialog.java
+++ com.vernalis.knime/src/com/vernalis/nodes/collection/size/CollectionSizeNodeDialog.java
@@ -1,35 +1,77 @@
 /*******************************************************************************
- * Copyright (c) 2019,2023, Vernalis (R&D) Ltd
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License, Version 3, as 
- *  published by the Free Software Foundation.
- *  
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of 
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
- *  See the GNU General Public License for more details.
- *   
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, see <http://www.gnu.org/licenses>
+ * Copyright (c) 2019, 2024, Vernalis (R&D) Ltd
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, Version 3, as
+ * published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>
  ******************************************************************************/
 package com.vernalis.nodes.collection.size;
 
+import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
+import org.knime.core.node.defaultnodesettings.DialogComponentButtonGroup;
+import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+
+import com.vernalis.knime.dialog.components.DialogComponentEmpty;
 import com.vernalis.nodes.collection.abstrct.AbstractMultiCollectionNodeDialog;
 
 /**
  * Node Dialog for the Collection Size node
  * 
  * @author S.Roughley knime@vernalis.com
- *
  */
 public class CollectionSizeNodeDialog
-		extends AbstractMultiCollectionNodeDialog {
+        extends AbstractMultiCollectionNodeDialog {
 
-	/**
-	 * Constructor
-	 */
-	public CollectionSizeNodeDialog() {
-		super(false);
-	}
+    private static final String UNIQUE_VALUES_ONLY = "Unique values only";
+
+    /**
+     * Constructor
+     */
+    public CollectionSizeNodeDialog() {
+
+        super(false);
+
+        createNewGroup("Counting options");
+
+        addDialogComponent(
+                new DialogComponentButtonGroup(createMissingValuesModel(), null,
+                        false, MissingValueOptions.values()));
+        addDialogComponent(new DialogComponentBoolean(createUniqueValuesModel(),
+                UNIQUE_VALUES_ONLY));
+        addDialogComponent(new DialogComponentEmpty(createOutputTypeModel()));
+    }
+
+    /**
+     * @return settings model for the 'Long outputs' option. This option is
+     *             present only to preserve backwards compatibility for existing
+     *             nodes, and is not intended to be changed by the user
+     */
+    static final SettingsModelBoolean createOutputTypeModel() {
+
+        return new SettingsModelBoolean("Long outputs", true);
+    }
+
+    /**
+     * @return settings model for the 'Unique values only' option
+     */
+    static final SettingsModelBoolean createUniqueValuesModel() {
+
+        return new SettingsModelBoolean(UNIQUE_VALUES_ONLY, false);
+    }
+
+    /**
+     * @return settings model for the 'Missing Values' option
+     */
+    static final SettingsModelString createMissingValuesModel() {
+
+        return new SettingsModelString("Missing Values",
+                MissingValueOptions.getDefault().getActionCommand());
+    }
 
 }
--- com.vernalis.knime/src/com/vernalis/nodes/collection/size/CollectionSizeNodeModel.java
+++ com.vernalis.knime/src/com/vernalis/nodes/collection/size/CollectionSizeNodeModel.java
@@ -1,18 +1,20 @@
 /*******************************************************************************
- * Copyright (c) 2019,2023, Vernalis (R&D) Ltd
- *  This program is free software; you can redistribute it and/or modify it 
- *  under the terms of the GNU General Public License, Version 3, as 
- *  published by the Free Software Foundation.
- *  
- *  This program is distributed in the hope that it will be useful, but 
- *  WITHOUT ANY WARRANTY; without even the implied warranty of 
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
- *  See the GNU General Public License for more details.
- *   
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, see <http://www.gnu.org/licenses>
+ * Copyright (c) 2019, 2024, Vernalis (R&D) Ltd
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, Version 3, as
+ * published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>
  ******************************************************************************/
 package com.vernalis.nodes.collection.size;
+
+import static com.vernalis.nodes.collection.size.CollectionSizeNodeDialog.createMissingValuesModel;
+import static com.vernalis.nodes.collection.size.CollectionSizeNodeDialog.createOutputTypeModel;
+import static com.vernalis.nodes.collection.size.CollectionSizeNodeDialog.createUniqueValuesModel;
 
 import java.util.Arrays;
 
@@ -24,51 +26,108 @@ import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.DataType;
 import org.knime.core.data.collection.CollectionDataValue;
 import org.knime.core.data.def.IntCell;
+import org.knime.core.data.def.LongCell;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
 
+import com.vernalis.knime.misc.ArrayUtils;
 import com.vernalis.nodes.collection.abstrct.AbstractMultiCollectionNodeModel;
-
 /**
  * NodeModel for the Collection Size node
  * 
  * @author S.Roughley knime@vernalis.com
- *
  */
 public class CollectionSizeNodeModel extends AbstractMultiCollectionNodeModel {
 
-	/**
-	 * Constructor
-	 *
-	 * @since 1.36.2
-	 */
-	protected CollectionSizeNodeModel() {
-		super(false);
-	}
+    private final SettingsModelString missingValuesMdl = registerSettingsModel(
+            createMissingValuesModel(), 3, mdl -> mdl.setStringValue(
+                    MissingValueOptions.All.getActionCommand()));
+    private final SettingsModelBoolean uniqueValuesMdl = registerSettingsModel(
+            createUniqueValuesModel(), 3, mdl -> mdl.setBooleanValue(false));
 
-	@Override
-	protected DataCell[] getCells(int[] idx, DataRow row,
-			DataColumnSpec[] newColSpecs) throws RuntimeException {
-		return Arrays.stream(idx).mapToObj(row::getCell)
-				.map(cell -> cell.isMissing() ? DataType.getMissingCell()
-						: new IntCell(((CollectionDataValue) cell).size()))
-				.toArray(DataCell[]::new);
-	}
+    // This model is not exposed in the configuration dialogue
+    // It simply serves to preserve the legacy 'int' counts for existing nodes
+    // whilst providing 'long' counts for new nodes
+    private final SettingsModelBoolean useLongsMdl = registerSettingsModel(
+            createOutputTypeModel(), 3,
+            mdl -> mdl.setBooleanValue(false));
 
-	@Override
-	protected DataColumnSpec[] createNewColumnSpecs(DataTableSpec spec,
-			int[] idx) {
+    private MissingValueOptions missingValueOpt;
 
-		return Arrays.stream(idx)
-				.mapToObj(i -> new DataColumnSpecCreator(
-						DataTableSpec.getUniqueColumnName(spec,
-								spec.getColumnSpec(i).getName() + " Size"),
-						IntCell.TYPE).createSpec())
-				.toArray(DataColumnSpec[]::new);
-	}
+    /**
+     * Constructor
+     *
+     * @since 10-Jan-2023
+     */
+    protected CollectionSizeNodeModel() {
 
-	@Override
-	protected boolean isReplaceInputCols() {
-		// We never replace the input columns for this node
-		return false;
-	}
+        // Version 3 - NB Version 2 is the current global default for the AMCNM
+        // - see super class
+        super(false, 3);
+    }
+
+    @Override
+    protected void doConfigure(DataTableSpec spec)
+            throws InvalidSettingsException {
+
+        try {
+            missingValueOpt = MissingValueOptions
+                    .valueOf(missingValuesMdl.getStringValue());
+        } catch (NullPointerException | IllegalArgumentException e) {
+            throw new InvalidSettingsException(
+                    "Unknown missing value handling option'"
+                            + missingValuesMdl.getStringValue() + "' - "
+                            + e.getMessage(),
+                    e);
+        }
+        super.doConfigure(spec);
+
+    }
+
+    @Override
+    protected DataCell[] getCells(int[] idx, DataRow row,
+            DataColumnSpec[] newColSpecs) throws RuntimeException {
+
+        DataCell[] retVal = ArrayUtils.fill(new DataCell[idx.length],
+                DataType.getMissingCell());
+        for (int i = 0; i < idx.length; i++) {
+            int colIdx = idx[i];
+            DataCell cell = row.getCell(colIdx);
+            if (cell.isMissing()) {
+                continue;
+            }
+            long l = missingValueOpt.getCount((CollectionDataValue) cell,
+                    uniqueValuesMdl.getBooleanValue());
+            if (useLongsMdl.getBooleanValue()) {
+                retVal[i] = new LongCell(l);
+            } else {
+                // We need to use IntCell here for backwards compatibility
+                retVal[i] = new IntCell((int) Math.min(Integer.MAX_VALUE, l));
+            }
+
+        }
+        return retVal;
+    }
+
+    @Override
+    protected DataColumnSpec[] createNewColumnSpecs(DataTableSpec spec,
+            int[] idx) {
+
+        return Arrays.stream(idx)
+                .mapToObj(i -> new DataColumnSpecCreator(
+                        DataTableSpec.getUniqueColumnName(spec,
+                                spec.getColumnSpec(i).getName() + " Size"),
+                        useLongsMdl.getBooleanValue() ? LongCell.TYPE
+                                : IntCell.TYPE).createSpec())
+                .toArray(DataColumnSpec[]::new);
+    }
+
+    @Override
+    protected boolean isReplaceInputCols() {
+
+        // We never replace the input columns for this node
+        return false;
+    }
 
 }
